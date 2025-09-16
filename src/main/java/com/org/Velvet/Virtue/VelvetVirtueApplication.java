04d64055eda8635b1d625c2c88240fe10687d911
AsitSwain2002 <147136555+AsitSwain2002@users.noreply.github.com>
2025-09-16 10:46:51 +0530

    first commit

---
+++ src/main/java/com/org/Velvet/Virtue/VelvetVirtueApplication.java
@@ -0,0 +1,13 @@
+package com.org.Velvet.Virtue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelvetVirtueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelvetVirtueApplication.class, args);
+	}
+
+}
