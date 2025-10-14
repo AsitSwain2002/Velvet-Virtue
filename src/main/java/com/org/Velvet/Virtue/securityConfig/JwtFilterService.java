f0298a523ca439331dab70b40ef629f509a00e6c
AsitSwain2002 <147136555+AsitSwain2002@users.noreply.github.com>
2025-10-14 20:54:30 +0530

    added custom validation for jwt expaired and invalid jwt token
    create 2 custom exception
    1) Invalid Token
    2) token is expaired

--- src/main/java/com/org/Velvet/Virtue/securityConfig/JwtFilterService.java
+++ src/main/java/com/org/Velvet/Virtue/securityConfig/JwtFilterService.java
@@ -3,6 +3,7 @@ package com.org.Velvet.Virtue.securityConfig;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,9 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.org.Velvet.Virtue.Util.GenericResponse;
 import com.org.Velvet.Virtue.service.JwtService;
 
 import jakarta.servlet.FilterChain;
@@ -28,30 +32,45 @@ public class JwtFilterService extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		String header = request.getHeader("Authorization");
+		try {
+			String header = request.getHeader("Authorization");
 
-		String userName = null;
-		String token = null;
-		if (header != null && header.startsWith("Bearer ")) {
-			token = header.substring(7);
-			userName = jwtService.extractUsername(token);
-
-		}
-		if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
-			UserDetails loadUserByUsername = detlServImpl.loadUserByUsername(userName);
-			boolean validateToken = jwtService.validateToken(token, loadUserByUsername);
-			if (validateToken) {
-				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-						loadUserByUsername, null, loadUserByUsername.getAuthorities());
-				usernamePasswordAuthenticationToken
-						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+			String userName = null;
+			String token = null;
+			if (header != null && header.startsWith("Bearer ")) {
+				token = header.substring(7);
+				userName = jwtService.extractUsername(token);
 
 			}
+			if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+				UserDetails loadUserByUsername = detlServImpl.loadUserByUsername(userName);
+				boolean validateToken = jwtService.validateToken(token, loadUserByUsername);
+				if (validateToken) {
+					UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+							loadUserByUsername, null, loadUserByUsername.getAuthorities());
+					usernamePasswordAuthenticationToken
+							.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+					SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+				}
+			}
+		} catch (Exception e) {
+			generateExpenceResponse(e, response);
+			return;
 		}
 		filterChain.doFilter(request, response);
+
+	}
+
+	private void generateExpenceResponse(Exception e, HttpServletResponse response)
+			throws JsonProcessingException, IOException {
+		response.setContentType("application/json");
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		Object errorMessage = GenericResponse.builder().msg(e.getMessage()).status(HttpStatus.UNAUTHORIZED.value())
+				.build().createWithOutData().getBody();
+		response.getWriter().write(new ObjectMapper().writeValueAsString(errorMessage));
 
 	}
 
